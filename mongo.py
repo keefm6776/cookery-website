660006853de2a2ee334adad121a15bd4d447249b
Ubuntu <ubuntu@ip-172-31-81-163.ec2.internal>
2019-06-24 21:12:37 +0000

    Initial Commit

---
+++ mongo.py
@@ -0,0 +1,22 @@
+import pymongo
+import os
+
+MONGODB_URI = os.getenv("MONGO_URI")
+DBS_NAME = "cookbook"
+COLLECTION_NAME = "recipes"
+            
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo has connected successfully!")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Unable to connect to MongoDB: %s") % e
+
+conn = mongo_connect(MONGODB_URI)
+coll = conn[DBS_NAME][COLLECTION_NAME]
+
+documents = coll.find()
+
+for doc in documents:
+    print(doc)
