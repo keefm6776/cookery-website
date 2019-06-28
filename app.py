3a6d3e58500fef1e84a4aed6180a03bae67f5a4f
Ubuntu <ubuntu@ip-172-31-81-163.ec2.internal>
2019-06-28 07:29:58 +0000

    attempting to simply display recipe

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'cookbook'
+app.config["MONGO_URI"] = 'mongodb+srv://root:31Wirpbj6677@cookery-website-xysxa.mongodb.net/cookbook?retryWrites=true&w=majority'
+
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", recipes = mongo.db.recipes.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-    port=int(os.environ.get('PORT')),
-    debug=True)
+            port=int(os.environ.get('PORT')),
+            debug=True)
--- mongo.py
+++ mongo.py
@@ -4,7 +4,7 @@ import os
 MONGODB_URI = os.getenv("MONGO_URI")
 DBS_NAME = "cookbook"
 COLLECTION_NAME = "recipes"
-            
+
 def mongo_connect(url):
     try:
         conn = pymongo.MongoClient(url)
@@ -12,7 +12,7 @@ def mongo_connect(url):
         return conn
     except pymongo.errors.ConnectionFailure as e:
         print("Unable to connect to MongoDB: %s") % e
-
+       
 conn = mongo_connect(MONGODB_URI)
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
@@ -20,3 +20,6 @@ documents = coll.find()
 
 for doc in documents:
     print(doc)
+    
+    
+    
