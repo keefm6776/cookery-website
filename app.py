99f03313e4e19560fbed45e5def12548919a3e67
Ubuntu <ubuntu@ip-172-31-81-163.ec2.internal>
2019-06-30 16:38:06 +0000

    Added Basic Add-Recipe Form

--- app.py
+++ app.py
@@ -12,8 +12,11 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_recipes')
 def get_recipes():
-    print(mongo.db.recipes.find())
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
+
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template("add_recipe.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
