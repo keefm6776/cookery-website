ffd434fdd06429387b27f3f3e0b81e2c651044fe
Ubuntu <ubuntu@ip-172-31-81-163.ec2.internal>
2019-06-30 15:23:20 +0000

    Display each recipe as a card on the main page

--- static/js/init.js
+++ static/js/init.js
@@ -3,6 +3,13 @@
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+    
+ 
+  });
+  });
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+
+   $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+  
